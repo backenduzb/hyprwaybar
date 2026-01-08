70c9efdcbcb437556d2149a8c13bdf616aa6bc50
Javohir <qobiljonsharipov33@gmail.com>
2026-01-08 10:18:44 +0500

    Added label

--- src/hypr/connector.rs
+++ src/hypr/connector.rs
@@ -1,16 +1,46 @@
 use crate::config::settings::BAR_HEIGHT;
+use wayland_client::protocol::wl_shm_pool;
+use std::ffi::CString;
+use std::os::fd::BorrowedFd;
+
+use std::{
+    fs::File,
+    os::unix::io::{AsRawFd, FromRawFd},
+    ptr,
+};
 
 use wayland_client::{
-    protocol::{wl_compositor, wl_registry, wl_surface},
-    Connection, Dispatch, QueueHandle, globals::{registry_queue_init, GlobalListContents, BindError},
+    protocol::{
+        wl_buffer, wl_compositor, wl_registry, wl_shm, wl_surface,
+    },
+    Connection, Dispatch, QueueHandle,
+    globals::{registry_queue_init, GlobalListContents},
 };
+
 use wayland_protocols_wlr::layer_shell::v1::client::{
     zwlr_layer_shell_v1::{self, ZwlrLayerShellV1},
     zwlr_layer_surface_v1::{self, Anchor, ZwlrLayerSurfaceV1},
 };
 
+use cairo::{Context, Format, ImageSurface};
+
 #[derive(Default)]
-struct State;
+struct State {
+    configured: bool,
+    width: i32,
+    height: i32,
+}
+
+impl Dispatch<wl_shm_pool::WlShmPool, ()> for State {
+    fn event(
+        _: &mut Self,
+        _: &wl_shm_pool::WlShmPool,
+        _: wl_shm_pool::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<Self>,
+    ) {}
+}
 
 impl Dispatch<wl_registry::WlRegistry, GlobalListContents> for State {
     fn event(
@@ -20,8 +50,7 @@ impl Dispatch<wl_registry::WlRegistry, GlobalListContents> for State {
         _: &GlobalListContents,
         _: &Connection,
         _: &QueueHandle<Self>,
-    ) {
-    }
+    ) {}
 }
 
 impl Dispatch<wl_compositor::WlCompositor, ()> for State {
@@ -30,6 +59,14 @@ impl Dispatch<wl_compositor::WlCompositor, ()> for State {
 
 impl Dispatch<wl_surface::WlSurface, ()> for State {
     fn event(_: &mut Self, _: &wl_surface::WlSurface, _: wl_surface::Event, _: &(), _: &Connection, _: &QueueHandle<Self>) {}
+}
+
+impl Dispatch<wl_shm::WlShm, ()> for State {
+    fn event(_: &mut Self, _: &wl_shm::WlShm, _: wl_shm::Event, _: &(), _: &Connection, _: &QueueHandle<Self>) {}
+}
+
+impl Dispatch<wl_buffer::WlBuffer, ()> for State {
+    fn event(_: &mut Self, _: &wl_buffer::WlBuffer, _: wl_buffer::Event, _: &(), _: &Connection, _: &QueueHandle<Self>) {}
 }
 
 impl Dispatch<ZwlrLayerShellV1, ()> for State {
@@ -43,35 +80,32 @@ impl Dispatch<ZwlrLayerSurfaceV1, ()> for State {
         event: zwlr_layer_surface_v1::Event,
         _: &(),
         _: &Connection,
-        qh: &QueueHandle<Self>,
+        _: &QueueHandle<Self>,
     ) {
         if let zwlr_layer_surface_v1::Event::Configure { serial, width, height } = event {
             layer_surface.ack_configure(serial);
+            state.width = if width == 0 { 1920 } else { width as i32 };
+            state.height = BAR_HEIGHT as i32;
+            state.configured = true;
         }
     }
 }
 
 pub fn run_connector() {
-    use cairo::{Context, Format, ImageSurface};
-    use std::convert::TryInto;
-
-    let conn = Connection::connect_to_env().expect("Wayland ga ulanish muvaffaqiyatsiz");
-
-    let (globals, mut event_queue) = registry_queue_init::<State>(&conn)
-        .expect("Registry ni ishga tushirib bo'lmadi");
-
+    let conn = Connection::connect_to_env().unwrap();
+    let (globals, mut event_queue) = registry_queue_init::<State>(&conn).unwrap();
     let qh = event_queue.handle();
 
-    let compositor: wl_compositor::WlCompositor = globals
-        .bind::<wl_compositor::WlCompositor, _, _>(&qh, 4..=6, ())
-        .expect("wl_compositor topilmadi");
+    let compositor: wl_compositor::WlCompositor =
+        globals.bind(&qh, 4..=6, ()).unwrap();
 
-    let layer_shell: ZwlrLayerShellV1 = globals
-        .bind::<ZwlrLayerShellV1, _, _>(&qh, 1..=4, ())
-        .expect("zwlr_layer_shell_v1 topilmadi");
+    let shm: wl_shm::WlShm =
+        globals.bind(&qh, 1..=1, ()).unwrap();
+
+    let layer_shell: ZwlrLayerShellV1 =
+        globals.bind(&qh, 1..=4, ()).unwrap();
 
     let surface = compositor.create_surface(&qh, ());
-
     let layer_surface = layer_shell.get_layer_surface(
         &surface,
         None,
@@ -80,38 +114,90 @@ pub fn run_connector() {
         &qh,
         (),
     );
-
     layer_surface.set_anchor(Anchor::Top | Anchor::Left | Anchor::Right);
     layer_surface.set_size(0, BAR_HEIGHT);
     layer_surface.set_exclusive_zone(BAR_HEIGHT as i32);
     surface.commit();
 
-    // Cairo surface yaratish (width va height i32 bo'lishi kerak)
-    let width: i32 = 1920;
-    let height: i32 = BAR_HEIGHT.try_into().unwrap();
+    let mut state = State::default();
 
-    let surface_cairo = ImageSurface::create(Format::ARgb32, width, height)
-        .expect("Cairo surface yaratilmadi");
-    let cr = Context::new(&surface_cairo).expect("Cairo context yaratilmadi");
+    while !state.configured {
+        event_queue.blocking_dispatch(&mut state).unwrap();
+    }
 
-    // Fon
+    let stride = state.width * 4;
+    let size = stride * state.height;
+    let name = CString::new("bar-buffer").unwrap();
+
+    let memfd = unsafe {
+        libc::syscall(
+            libc::SYS_memfd_create,
+            name.as_ptr(),
+            0,
+        ) as i32
+    };
+    assert!(memfd >= 0);
+
+    unsafe {
+        libc::ftruncate(memfd, size as i64);
+    }
+
+    let data = unsafe {
+        libc::mmap(
+            ptr::null_mut(),
+            size as usize,
+            libc::PROT_READ | libc::PROT_WRITE,
+            libc::MAP_SHARED,
+            memfd,
+            0,
+        )
+    } as *mut u8;
+
+    let file = unsafe { File::from_raw_fd(memfd) };
+    let fd = unsafe { BorrowedFd::borrow_raw(file.as_raw_fd()) };
+
+    let pool = shm.create_pool(fd, size, &qh, ());
+    let buffer = pool.create_buffer(
+        0,
+        state.width,
+        state.height,
+        stride,
+        wl_shm::Format::Argb8888,
+        &qh,
+        (),
+    );
+
+    let cairo_surface = unsafe {
+        ImageSurface::create_for_data(
+            std::slice::from_raw_parts_mut(data, size as usize),
+            Format::ARgb32,
+            state.width,
+            state.height,
+            stride,
+        ).unwrap()
+    };
+
+    let cr = Context::new(&cairo_surface).unwrap();
+
     cr.set_source_rgb(0.0, 0.0, 0.0);
-    cr.paint().expect("Cairo paint xatosi");
+    cr.paint().unwrap();
 
-    // Text
-    cr.set_source_rgb(0.0, 1.0, 0.0); // yashil
-    cr.select_font_face("Sans", cairo::FontSlant::Normal, cairo::FontWeight::Bold);
+    cr.set_source_rgb(0.0, 1.0, 0.0);
+    cr.select_font_face(
+        "Sans",
+        cairo::FontSlant::Normal,
+        cairo::FontWeight::Bold,
+    );
     cr.set_font_size(20.0);
-    cr.move_to(10.0, 22.0);
-    cr.show_text("Salom, bu Rust Wayland bar!").expect("Cairo show_text xatosi");
+    cr.move_to(10.0, 24.0);
+    cr.show_text("Salom, Rust Wayland bar!").unwrap();
 
-    surface_cairo.flush();
+    cairo_surface.flush();
 
-    // Wayland event loop
+    surface.attach(Some(&buffer), 0, 0);
+    surface.commit();
+
     loop {
-        if let Err(e) = event_queue.blocking_dispatch(&mut State::default()) {
-            eprintln!("Dispatch xatosi: {e:?}");
-            break;
-        }
+        event_queue.blocking_dispatch(&mut state).unwrap();
     }
 }
